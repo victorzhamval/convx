217e1ea20efeb216b84fc18f65518016578d87be
Victor Zhamval <victorzhamval@gmail.com>
2024-12-06 17:21:17 -0500

    Fix output

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,7 +1,7 @@
 export default function Output({children}) {
   return (
     <div
-      className="w-full h-11 flex items-center bg-transparent pl-3.5 text-[#656565] bg-gray-200 rounded focus:outline-none appearance-none placeholder:text-[#9D9D9D]"
+      className="w-full h-11 flex items-center pl-3.5 text-[#656565] bg-gray-200 rounded focus"
     >
     {children}
     </div>
